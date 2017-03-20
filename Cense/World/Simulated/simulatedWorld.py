f8ca9c560047bcb0b3be6ee9a25e7d6f5c7ffac1
lukas <lukas.szimtenings@rwth-aachen.de>
2017-03-20 10:17:08 +0100

    Made state size variable Added comments

--- Cense/World/Simulated/simulatedWorld.py
+++ Cense/World/Simulated/simulatedWorld.py
@@ -9,6 +9,8 @@ class SimulatedWorld(World):
     __world = np.zeros([5, 5])
     # Current position of the tool center point
     tcp_pos = 0, 0
+    # Static variable determining how big a state snapshot should be
+    __state_size = 9
 
     def __init__(self, world_path=None):
         if world_path is not None:
@@ -23,16 +25,13 @@ class SimulatedWorld(World):
     # Initiates the claws and the first goal
     #
     def __create_init_state(self):
-
-        # Determine size of the state
-        state_size = 9
-
         # Calculate the offset of the claws from the center point of a given state
-        tool_offset = (state_size-1)/2
+        tool_offset = (self.__state_size-1)/2
+        self.tcp_pos = tool_offset, 0
 
         # Calculate worlds center based on the assumption that the cable always starts in the middle of the image
         center = (self.__world.shape[0]-1)/2
-        if center < 3:
+        if center < self.__state_size:
             print("Warning: image is to small to properly position claws")
 
         # Calculate initial claw positions based on the worlds center and the previously calculated offset
@@ -41,17 +40,33 @@ class SimulatedWorld(World):
         self._set_flag(upper_claw_pos, self.rotor_top)
         self._set_flag(lower_claw_pos, self.rotor_bot)
 
+        # Starting position of line is always in the exact middle (Might change some day)
+        lineposx = self.tcp_pos.index(0)
+        lineposy = self.tcp_pos.index(1)
+
+    #
+    # Moves the claws one unit to the right
+    #
     def __move_right(self):
         pass
 
+    #
+    # Sets a flag while maintaining all other flags that have been set
+    #
     def _add_flag(self, position, state):
         if not self._flag_is_set(position, state):
             self.__world[position] += state
 
+    #
+    # Removes a flag if it is set
+    #
     def _remove_flag(self, position, state):
         if self._flag_is_set(position, state):
             self.__world[position] -= state
 
+    #
+    # Sets a flag while removing all other set flags
+    #
     def _set_flag(self, position, state):
         self.__world[position] = state
 
@@ -70,3 +85,9 @@ class SimulatedWorld(World):
             return True
         else:
             return False
+
+    def find_new_goal(self, old_goal):
+        line_pos_x = old_goal.index(0)
+
+        # Find the position where the line goes from the penultimate column to the last column
+
