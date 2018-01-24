3835318265e517c51269f8421bb5411e706d402a
cs671660 <Y#Imaany4$ZlwK>
2018-01-24 13:05:34 +0100

    Fixed interface layout

--- Cense/Interface/interface.py
+++ Cense/Interface/interface.py
@@ -200,9 +200,9 @@ class CockpitWindow(QtWidgets.QMainWindow):
         #layout.addWidget(steps_widget, 1, 1)
         layout.addWidget(state_plot_widget, 1, 1)
         #layout.addWidget(exploration_widget, 1, 3)
-        #layout.addWidget(test_steps_widget, 2, 1)
+        layout.addWidget(test_steps_widget, 2, 1)
         layout.addWidget(action_widget, 1, 2)
-        #layout.addWidget(self.text_widget, 2, 3)
+        layout.addWidget(self.text_widget, 2, 2)
 
         self.agent.steps_signal.connect(self.update_steps)
         self.agent.state_signal.connect(self.update_state)
